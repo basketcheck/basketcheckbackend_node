47771e6c7f7aaef232b27765853cd55297111766
noahmik <s46422574@gmail.com>
2023-12-26 13:41:07 +0900

    DOCS :: shuffle 구현

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -67,4 +67,31 @@ router.get('/join/:name', async (req, res) => {
     }
   });  
   
+  router.get('/shuffle', async (req, res) => {
+    try {
+      // Vote 테이블에서 모든 레코드 조회
+      const votes = await Vote.findAll();
+  
+      // 조회된 레코드를 랜덤하게 섞음
+      const shuffledVotes = shuffleArray(votes);
+  
+      // 레코드를 두 팀으로 나눔
+      const team1 = shuffledVotes.slice(0, Math.ceil(shuffledVotes.length / 2));
+      const team2 = shuffledVotes.slice(Math.ceil(shuffledVotes.length / 2));
+  
+      res.status(200).json({ team1, team2 });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: '조회 도중 오류가 발생했습니다.' });
+    }
+  });
+  
+  // 배열을 랜덤하게 섞는 함수
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 module.exports = router;
