0bc424f02c73b79a3bbc2aa5b2cbc30c1cf23e2e
noahmik <s46422574@gmail.com>
2023-12-26 13:13:40 +0900

    DOCS :: starting 구현

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -2,9 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const Vote = require("../models/Vote");
 const router = express.Router();
+const { Op } = require('sequelize');
+
 
 //join
-router.get('/:name', async (req, res) => {
+router.get('/join/:name', async (req, res) => {
     try {
       // 요청으로부터 name 파라미터 추출
       const { name } = req.params;
@@ -21,6 +23,29 @@ router.get('/:name', async (req, res) => {
       console.error(error);
       res.status(500).json({ message: '투표 생성 도중 오류가 발생했습니다.' });
     }
-  });    
+  });
+  
+  // //starting
+  router.get('/starting', async (req, res) => {
+    try {
+      // Vote 테이블에서 Votenum≤10인 name들을 조회
+      const votes = await Vote.findAll({
+        where: {
+          Votenum: {
+            [Op.lte]: 10
+          }
+        },
+        attributes: ['name']
+      });
+  
+      res.status(200).json({ votes });
+    } catch (error) {
+      // 오류 처리
+      console.error(error);
+      res.status(500).json({ message: '조회 도중 오류가 발생했습니다.' });
+    }
+  });
 
+    
+  
 module.exports = router;
