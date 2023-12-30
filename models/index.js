d05c06e99d78a25d7c118bdc03bfe5b92b641f75
noahmik <s46422574@gmail.com>
2023-12-31 00:55:39 +0900

    REFACTOR :: index.js 수정

--- models/index.js
+++ models/index.js
@@ -6,9 +6,18 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  dialect: config.dialect,
-});
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    operatorsAliases: false,
+    timezone: '+09:00',
+    port: config.port,
+  }
+);
 
 db.sequelize = sequelize;
 
