804d6f0878f4d173199fca958c0f70abe3c066a4
noahmik <s46422574@gmail.com>
2024-01-10 19:48:29 +0900

    cors 재수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cron = require('node-cron');
 const app = express();
 app.use(
 	cors({
-		origin: 'http://localhost:3000/',
+		origin: 'http://localhost:3000',
 		credentials: true,
 		withCredentials: true,
 		optionsSuccessStatus: 200,
