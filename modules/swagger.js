4caa7c37da519eb7e65dac85eedb78d77e5d29dd
noahmik <s46422574@gmail.com>
2024-01-10 22:06:53 +0900

    FIX :: swagger module 수정

---
+++ modules/swagger.js
@@ -0,0 +1,22 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggereJsdoc = require('swagger-jsdoc');
+
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'Test API',
+            version: '1.0.0',
+            description: 'Test API with express',
+        },
+        host: 'https://port-0-basketcheckbackend-node-9zxht12blq9gr7pi.sel4.cloudtype.app',
+        basePath: '/'
+    },
+    apis: ['./routes/*.js', './swagger/*']
+};
+
+const specs = swaggereJsdoc(options);
+
+module.exports = {
+    swaggerUi,
+    specs
+};
